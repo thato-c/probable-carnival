be90700d08e56775ad7a43fdabb7c23e07ce6c79
Thato <t.chitja369@gmail.com>
2023-11-04 10:53:28 +0200

    Add Validation to all Entities

--- ProductManager/ProductManager/Models/Company.cs
+++ ProductManager/ProductManager/Models/Company.cs
@@ -1,10 +1,22 @@
-﻿namespace ProductManager.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductManager.Models
 {
     public class Company
     {
+        [Key]
         public int CompanyId { get; set; }
+
+        [Required(ErrorMessage = "Company Name is required")]
+        [StringLength(50)]
         public string CompanyName { get; set; }
+
+        [Required(ErrorMessage = "Company Name is required")]
+        [StringLength(15)]
         public string CompanyPhoneNumber { get; set;} = string.Empty;
+
+        [Required(ErrorMessage = "Company Name is required")]
+        [EmailAddress]
         public string CompanyEmail { get; set; } = string.Empty;
         public ICollection<LicencePurchase> LicencePurchase { get; set; }
     }
--- ProductManager/ProductManager/Models/Licence.cs
+++ ProductManager/ProductManager/Models/Licence.cs
@@ -1,15 +1,28 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ProductManager.Models
 {
     public class Licence
     {
+        [Key]
         public int LicenceId { get; set; }
+
+        [Required(ErrorMessage = "Licence Name is required")]
+        [StringLength(50)]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Licence Name is required")]
+        [StringLength(100)]
         public string Description { get; set; }
 
+        [Required(ErrorMessage = "Licence Cost is required")]
         [Column(TypeName = "decimal(10, 2")]
         public decimal Cost { get; set; }
+
+        [Required(ErrorMessage = "Licence Duration is required")]
+        [Range(1, int.MaxValue)]
+        public int ValidityMonths { get; set; } = 1;
 
         public ICollection<LicencePurchase> LicencePurchase { get; set; }
     }
