e06c794fad4e3534857b9aace642d75a091afe6b
Thato <t.chitja369@gmail.com>
2023-11-04 10:22:42 +0200

    Add Licence Entity

---
+++ ProductManager/ProductManager/Models/Licence.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductManager.Models
+{
+    public class Licence
+    {
+        public int LicenceId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        [Column(TypeName = "decimal(10, 2")]
+        public decimal Cost { get; set; }
+    }
+}
