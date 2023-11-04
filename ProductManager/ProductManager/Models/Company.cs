832f92413c07f086f5dc6765b80bd4478d7c54a7
Thato <t.chitja369@gmail.com>
2023-11-04 10:20:40 +0200

    Add Company Entity

---
+++ ProductManager/ProductManager/Models/Company.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductManager.Models
+{
+    public class Company
+    {
+        public int CompanyId { get; set; }
+        public string CompanyName { get; set; }
+        public string CompanyPhoneNumber { get; set;} = string.Empty;
+        public string CompanyEmail { get; set; } = string.Empty;
+    }
+}
