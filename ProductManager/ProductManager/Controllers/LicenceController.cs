c2fb9fd942ca38130fc5a70dd9ecdf0d2e1dd7f8
Thato <t.chitja369@gmail.com>
2023-11-04 12:15:56 +0200

    Add Create functionality to Licence

--- ProductManager/ProductManager/Controllers/LicenceController.cs
+++ ProductManager/ProductManager/Controllers/LicenceController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ProductManager.Data;
 using ProductManager.Models;
+using ProductManager.ViewModels;
 
 namespace ProductManager.Controllers
 {
@@ -32,6 +33,37 @@ namespace ProductManager.Controllers
             }
 
             return View(licences);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(LicenceViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Map the ViewModel to Licence entity
+                var Licence = new Models.Licence
+                {
+                    Name = model.Name,
+                    Description = model.Description,
+                    Cost = model.Cost,
+                    ValidityMonths = model.ValidityMonths
+                };
+
+                // Add and save the new licence to the database
+                _context.Licences.Add(Licence);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
         }
     }
 }
