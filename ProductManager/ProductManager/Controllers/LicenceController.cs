5f940b8656e4e29c7aa17714a46d802655d2289e
Thato <t.chitja369@gmail.com>
2023-11-04 11:57:47 +0200

    Add Licence Index Page

---
+++ ProductManager/ProductManager/Controllers/LicenceController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProductManager.Data;
+using ProductManager.Models;
+
+namespace ProductManager.Controllers
+{
+    public class LicenceController : Controller
+    {
+        private readonly ApplicationDBContext _context;
+
+        public LicenceController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Licences
+        public async Task<IActionResult> Index()
+        {
+            // Retrieve a list of licences from the database
+            var licences = await _context.Licences.ToListAsync();
+
+            if (licences.Count == 0)
+            {
+                ViewBag.Message = "No Licences Registered";
+                return View();
+            }
+
+            return View(licences);
+        }
+    }
+}
