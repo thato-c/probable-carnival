a9970ca1fb8690db90a2c410ab9aed22d13f511b
Thato <t.chitja369@gmail.com>
2023-11-06 10:38:21 +0200

    Add LicencePurchase Form Fields

--- ProductManager/ProductManager/Controllers/LicencePurchaseController.cs
+++ ProductManager/ProductManager/Controllers/LicencePurchaseController.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NuGet.Packaging.Licenses;
+using ProductManager.Data;
+using ProductManager.ViewModels;
 
 namespace ProductManager.Controllers
 {
     public class LicencePurchaseController : Controller
     {
+
+        private readonly ApplicationDBContext _context;
+
+        public LicencePurchaseController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var viewModel = new LicencePurchaseViewModel();
+            return View(viewModel);
         }
     }
 }
---
+++ ProductManager/ProductManager/ViewModels/LicencePurchaseViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductManager.ViewModels
+{
+    public class LicencePurchaseViewModel
+    {
+        public CompanyViewModel CompanyDetails { get; set; }
+        public LicenceViewModel LicenceDetails { get; set; }
+
+        // Licence purchase information
+        [Required(ErrorMessage = "Licence Quantity is required")]
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be greater than or equal to 1")]
+        public int Quantity { get; set; }
+
+        [Display(Name = "Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Required(ErrorMessage = "Purchase Date is required")]
+        public DateTime PurchaseDate { get; set; } = DateTime.Now;
+
+        [Column(TypeName = "decimal(10, 2)")]
+        [Range(0, double.MaxValue, ErrorMessage = "Total Cost must be a non-negative value")]
+        public decimal TotalCost { get; set; }
+    }
+}
