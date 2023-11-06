5a67fd43b1e52884a363324a8ab64ad78eeed83b
Thato <t.chitja369@gmail.com>
2023-11-06 08:57:19 +0200

    Add LicencePurchase Index Controller and View

---
+++ ProductManager/ProductManager/Controllers/LicencePurchaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductManager.Controllers
+{
+    public class LicencePurchaseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
