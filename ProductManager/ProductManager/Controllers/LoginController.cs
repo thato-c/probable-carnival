a53d7c8716a8b024b9304f88663f09f3a5fc3345
Thato <t.chitja369@gmail.com>
2023-12-05 21:55:55 +0200

    Create Login Feature

---
+++ ProductManager/ProductManager/Controllers/LoginController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProductManager.Data;
+using ProductManager.ViewModels;
+
+namespace ProductManager.Controllers
+{
+    public class LoginController : Controller
+    {
+        public readonly ApplicationDBContext _context;
+
+        public LoginController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _context.Users.FirstOrDefaultAsync(u =>
+                        u.Username == model.Username);
+
+                if (user != null && BCrypt.Net.BCrypt.Verify(model.Password, user.Password))
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View("Index", model);
+        }
+    }
+}
---
+++ ProductManager/ProductManager/ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductManager.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Username is required")]
+        [EmailAddress]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
